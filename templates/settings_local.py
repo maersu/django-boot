9d710a7ce9953962888b6813ae3ef60f41f54261
Marcel Eyer <maersu@judges.ch>
2011-03-22 10:10:24 +0100

    settings_local added

---
+++ templates/settings_local.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import logging.config
+import os
+import socket
+import sys
+
+DEBUG = True
+
+USE_DEBUG_TOOLBAR = True
+
+INTERNAL_IPS = ('127.0.0.1', socket.gethostbyname('app.dev'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',             # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '../../db/{{projectname}}.db',       # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
