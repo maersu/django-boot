8bee07fb945e9e90e5756b4405d2a12cdf0b5438
Marcel Eyer <maersu@judges.ch>
2011-03-23 11:10:52 +0100

    added settings_local other django github repo

---
+++ templates/settings_local.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Atizo - The Open Innovation Platform
+# http://www.atizo.com/
+#
+# Copyright (c) 2008-2010 Atizo AG. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
+
+ADMINS = (
+     #
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '../../db/whp.db',                      # Or path to database file if using sqlite3.
+        'USER': 'atizo_user',                      # Not used with sqlite3.
+        'PASSWORD': 'test',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+DEBUG = True
+USE_DEBUG_TOOLBAR = False
+
